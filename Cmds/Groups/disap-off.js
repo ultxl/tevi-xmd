e4e05172223778e7af56c0706d1ae21b771ac18e
AmTevi <140701204+ultxl@users.noreply.github.com>
2025-03-22 11:07:14 +0300

    Add files via upload

--- Cmds/Groups/disap-off.js
+++ Cmds/Groups/disap-off.js
@@ -4,9 +4,7 @@ module.exports = async (context) => {
     await middleware(context, async () => {
         const { client, m, sendReply, sendMediaMessage } = context;
 
-if (!m.quoted) return sendReply(client, m, `Did you quote a message ?`); 
-
-client.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.quoted.id, participant: m.quoted.sender } }); 
-  });
-
-}
+        await client.groupToggleEphemeral(m.chat, 0);
+        sendReply(client, m, 'Disappearing messages successfully turned off!');
+    });
+};
